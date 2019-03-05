142a5a023391e3d87aebcb06f60603e333815dcb
mattorus <mattorus@gmail.com>
2019-03-04 20:22:41 -0800

    Add payment with Stripe.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -38,7 +38,7 @@ class ConfirmationsController < Milia::ConfirmationsController
       self.resource = resource_class.confirm_by_token(params[:confirmation_token])
       yield resource if block_given?
       
-      if resource.errors.empty
+      if resource.errors.empty?
         set_flash_message(:notice, :confirmed) if is_flashing_format?
       end
 
